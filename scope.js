b6a3cd9b1862ba7ff6fdde7993ef23a3d4624e84
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-05-06 11:41:25 -0400

    First attempt at the problem

---
+++ scope.js
@@ -0,0 +1,16 @@
+// Coded by Jared Smith
+// scope.js problem
+// CMP237 FINAL
+
+var foo = function() {
+  var x = "This ";
+  var y = "works!";
+  console.log(x);
+  console.log(y); 
+}
+
+var x = 10
+
+var y = 9
+
+console.log(foo());
