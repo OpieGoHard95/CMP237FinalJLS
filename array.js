70467972120969ad93fa9c779987a1c05fcb9967
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-05-06 12:11:33 -0400

    Filters out the red fruits from the rest

--- array.js
+++ array.js
@@ -10,3 +10,16 @@ var fruitList = [
 ];
 
 console.log(fruitList);
+
+var filter = function(array, test) {
+	var passed = [];
+	for (var j = 0; j < array.length; j = j + 1) {
+		if (test(array[j]))
+			passed.push(array[j]);
+		}
+		return passed;
+	}
+
+console.log(filter(fruitList, function(fruit) {
+	return fruit.color == "red";
+	}));
