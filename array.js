2ecd363044ccec86c9f0ac36dfe26b0d1553cab3
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-05-06 12:06:50 -0400

    Added the array that I'm going to filter`

---
+++ array.js
@@ -0,0 +1,12 @@
+// Coded by Jared Smith
+// Array problem
+// CMP237 FINAL
+
+var fruitList = [
+{ "name": "banana", "color": "yellow" },
+{ "name": "apple", "color": "red" },
+{ "name": "strawberry", "color": "red" },
+{ "name": "peach", "color": "orange" },
+];
+
+console.log(fruitList);
